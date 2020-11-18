5078ff8fe075093bec75cd1044991608ff7cc087
AbdulSamad Ayoade <ayoadesamad@gmail.com>
2020-11-18 22:51:01 +0100

    define the variables

---
+++ app.js
@@ -0,0 +1,6 @@
+// DEFINE THE VARIABLES
+const form = document.getElementById('task-form');
+const taskList = document.querySelector('.collection');
+const taskInput = document.getElementById('task');
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
