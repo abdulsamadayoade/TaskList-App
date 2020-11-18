10713dd36549d8f9db506e1caa81e5022d61d10f
AbdulSamad Ayoade <ayoadesamad@gmail.com>
2020-11-18 22:53:07 +0100

    add event listeners

--- app.js
+++ app.js
@@ -4,3 +4,21 @@ const taskList = document.querySelector('.collection');
 const taskInput = document.getElementById('task');
 const clearBtn = document.querySelector('.clear-tasks');
 const filter = document.querySelector('#filter');
+
+// LOAD ALL EVENT LISTENERS
+loadEventListeners();
+
+// LOAD ALL EVENT LISTENERS
+
+function loadEventListeners() {
+    // DOM LOAD EVENT
+    document.addEventListener('DOMContentLoaded', getTasks);
+    // ADD TASK EVENT
+    form.addEventListener('submit', addTask);
+    // REMOVE TASK EVENT
+    taskList.addEventListener('click', removeTask);
+    // CLEAR TASK EVENT
+    clearBtn.addEventListener('click', clearTasks);
+    // FILTER TASKS
+    filter.addEventListener('keyup', filterTasks);
+}
